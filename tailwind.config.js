bdd994d0e275fc027d3bf05670ccd636f7d10815
Hansel <hauwmgames@gmail.com>
2025-11-03 23:04:57 +0100

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+// ⚠️ Auto-generated for IntelliSense only. Do not edit manually.
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+  "dark-grayish-blue": "hsl(217, 19%, 35%)",
+  "dark-blue": "hsl(214, 17%, 51%)",
+  "grayish-blue": "hsl(212, 23%, 69%)",
+  "light-grayish-blue": "hsl(210, 46%, 95%)"
+},
+      fontFamily: {
+  "sans": "\"Manrope\", sans-serif"
+},
+    },
+  },
+  plugins: [],
+};
