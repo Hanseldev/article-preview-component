8283a699c32e0e040947807904053dfe3295af4f
Hansel <hauwmgames@gmail.com>
2025-11-03 23:58:02 +0100

    Reset repo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '../dist/assets/index-D6lHtKlt.css'
+
+const shareOn = document.getElementById('share-on');
+const shareOff = document.getElementById('share-off');
+const authorInfo = document.getElementById('author-info');
+const socialInfo = document.getElementById('social-info');
+
+shareOn.addEventListener('click', () => {
+    authorInfo.classList.add('hidden');
+    socialInfo.classList.remove('hidden');
+})
+
+shareOff.addEventListener('click', () => {
+    socialInfo.classList.add('hidden');
+    authorInfo.classList.remove('hidden');
+})
